107d0284519317b30c1854fbea295492d1662842
damenkevin <kevindamen_1@hotmail.com>
2017-12-19 16:36:41 +0000

    Template and view working with db

--- crazy88quote/views.py
+++ crazy88quote/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Quote
 
 def post_list(request):
-    return render(request, 'crazy88quote/post_list.html')
+    quotes = Quote.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request, 'crazy88quote/post_list.html', {'quotes': quotes})
