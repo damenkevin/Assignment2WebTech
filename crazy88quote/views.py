f0a23b17805f844783922d09b5f79a1670193edc
damenkevin <kevindamen_1@hotmail.com>
2017-12-20 09:59:25 +0000

    Not working form added

--- crazy88quote/views.py
+++ crazy88quote/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Quote
+from .forms import QuoteForm
 
 def quote(request):
     quotes = Quote.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
     number = Quote.objects.count() % 2
-    return render(request, 'crazy88quote/quote.html', {'quotes': quotes, 'number':number})
+    form = QuoteForm()
+    return render(request, 'crazy88quote/quote.html', {'quotes': quotes, 'number':number, 'form':form})
 
 def crazy88(request):
 
