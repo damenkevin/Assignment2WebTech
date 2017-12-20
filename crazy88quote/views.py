06e51284be3ef288d6fb511bb19f3a9cc6f2eb5a
damenkevin <kevindamen_1@hotmail.com>
2017-12-20 11:58:39 +0000

    Working form that saves the quote

--- crazy88quote/views.py
+++ crazy88quote/views.py
@@ -4,9 +4,18 @@ from .models import Quote
 from .forms import QuoteForm
 
 def quote(request):
-    quotes = Quote.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    quotes = Quote.objects.filter(created_date__lte=timezone.now()).order_by('-created_date')
     number = Quote.objects.count() % 2
-    form = QuoteForm()
+    if request.method == "POST":
+        form = QuoteForm(request.POST)
+        if form.is_valid():
+            quote = form.save(commit=False)
+            quote.author = request.user
+            quote.published_date = timezone.now()
+            quote.save()
+            return render(request, 'crazy88quote/quote.html', {'quotes': quotes, 'number':number, 'form':form})
+    else:
+        form = QuoteForm()
     return render(request, 'crazy88quote/quote.html', {'quotes': quotes, 'number':number, 'form':form})
 
 def crazy88(request):
